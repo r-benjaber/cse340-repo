eae26c80b2350e4a7eb74b810375006bba6df1f5
r-benjaber <polbenjaber@gmail.com>
2026-05-22 17:06:47 -0300

    fix

--- src/models/db.js
+++ src/models/db.js
@@ -14,7 +14,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
     connectionString: process.env.DB_URL,
     ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: false
     }
 });
 
